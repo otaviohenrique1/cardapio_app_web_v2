6b9a25921999506094623936264c115998d2fb29
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-07-07 11:59:25 -0300

    commit 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,60 @@
+'use client'
+
+// import styles from "./page.module.css";
 import Image from "next/image";
-import styles from "./page.module.css";
+import { useState, useEffect } from "react";
+
+interface DataTypes {
+  id: string;
+  nome: string;
+  descricao: string;
+  preco: number;
+  tipo: string;
+  data_cadastro: Date | string;
+  foto: string;
+}
 
 export default function Home() {
+  const [data, setData] = useState<DataTypes[]>([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/produtos/")
+      .then((data) => data.json())
+      .then((data) => {
+        setData(data)
+      })
+      .catch((erro) => {
+        console.error("erro => ", erro);
+      })
+  }, [])
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <ol>
+        {data.map((item) => {
+          return (
+            <>
+              <li key={item.id}>
+                <ul>
+                  <li>{item.nome}</li>
+                  <li>{item.descricao}</li>
+                  <li>{item.preco}</li>
+                  <li>{(item.tipo == "B") ? "Bebida" : "Comida"}</li>
+                  <li>{item.data_cadastro.toString()}</li>
+                  <Image
+                    src={item.foto}
+                    alt={item.nome}
+                    width={60}
+                    height={60}
+                  />
+                  <li>{item.foto}</li>
+                </ul>
+              </li>
+              <hr />
+            </>
+          );
+        })}
+      </ol>
+    </div>
   );
 }
